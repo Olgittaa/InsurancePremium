f3f7b8cd751d72dfc137d94858b9e8eb9635bca7
Olha Charna <olgitta@olhas-mbp.speedport.ip>
2024-08-23 18:55:44 +0200

    Initial commit

---
+++ src/main/java/de/insurancepremium/InsurancePremiumApplication.java
@@ -0,0 +1,13 @@
+package de.insurancepremium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePremiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsurancePremiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/insurancepremium/InsurancePremiumApplicationTests.java
@@ -0,0 +1,13 @@
+package de.insurancepremium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancePremiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
